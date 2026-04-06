71344d6622ca30576d02cd80cc3bb570d8bebd02
dan-gheorghita <79468719+dan-gheorghita@users.noreply.github.com>
2026-04-06 09:56:21 +0300

    Add Python script

---
+++ allMyCats1.py
@@ -0,0 +1,10 @@
+print('Enter the name of cat 1:')  # Prompt user to enter the name of the first cat
+cat_name_1 = input()  # Store the input as a string
+print('Enter the name of cat 2:')  # Prompt user to enter the name of the second cat
+cat_name_2 = input()  # Store the input as a string
+print('Enter the name of cat 3:')  # Prompt user to enter the name of the third cat
+cat_name_3 = input()  # Store the input as a string
+print('Enter the name of cat 4:')  # Prompt user to enter the name of the fourth cat
+cat_name_4 = input()  # Store the input as a string
+print('The cat names are:')  # Display a message before printing the cat names
+print(cat_name_1 + ' ' + cat_name_2 + ' ' + cat_name_3 + ' ' + cat_name_4)  # Concatenate all cat names and print them
